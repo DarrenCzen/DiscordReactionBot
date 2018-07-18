6c16221f1a1dcb2d2e0abb52a936250b4ea6952a
Darren <zen_darren@hotmail.com>
2018-07-18 11:52:13 +0000

    Second Commit

---
+++ app.js
@@ -0,0 +1,47 @@
+﻿const discord = require("discord.js");
+
+const TOKEN = "Insert Token Here";
+const keyword = "Important Keyword Here";
+
+//Create and log in client
+const client = new discord.Client();
+client.login(TOKEN).then(() => {
+    console.log(`Logged in as ${client.user.tag}`);
+    registerListeners();
+}).catch(reason => {
+    console.log(`Problem while logging in bot: ${reason}`);
+    process.exit();
+});
+
+//Looks for emoji and registers a message handler
+function registerListeners() {
+    let emoji = '🛒';
+    client.on("message", message => {
+        console.log(message.content);
+        if (message.author.username === keyword) {
+            message.react(emoji).then(() => {
+                console.log("Reacted to message")
+            }).catch(reason => {
+                console.log(`Problem while reacting to message: ${reason}`);
+            });
+        }
+    });
+}
+
+//On process exit stuff
+const cleanupFunc = async (code) => {
+    await client.destroy();
+    process.exit(code);
+};
+
+process.once("exit", cleanupFunc);
+process.once("SIGINT", cleanupFunc);
+process.once("SIGTERM", cleanupFunc);
+process.once("unhandledRejection", (async (reason, promise) => {
+    console.log("Unhandled promise rejection at: Promise", promise, "reason:", reason);
+    await cleanupFunc(0);
+}));
+process.once("uncaughtException", async error => {
+    console.log(error.stack);
+    await cleanupFunc(0);
+});
